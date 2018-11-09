7efce2ab89ab8075ef194ea89210dd46423850d2
MrManz <eeNX2160>
2018-11-09 15:16:07 +0100

    Added static file serving

--- hello.go
+++ hello.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/go-martini/martini"
+	"net/http"
 )
 
 func main() {
 	m := martini.Classic()
-	static := martini.Static("public", martini.StaticOptions{IndexFile: "index.html"})
+	static := martini.Static("public", martini.StaticOptions{IndexFile: "index.html", Exclude: "/data"})
+	m.NotFound(static, http.NotFound)
 	m.Use(static)
+	m.Get("/data", func() string {
+		return "Hello AWS world!"
+	})
 	m.Run()
 }
